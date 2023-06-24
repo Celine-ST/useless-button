83bdc80cedc308a76781900f1d73e093033cbe4a
Celine-ST <celine.quek10@rafflesgirlssch.edu.sg>
2023-06-24 12:05:49 +0800

    Initial Commit

---
+++ useless button/useless_buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  useless_buttonApp.swift
+//  useless button
+//
+//  Created by Celine Quek on 24/6/23.
+//
+
+import SwiftUI
+
+@main
+struct useless_buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
