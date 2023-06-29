1806307d9b8b6e253cd46af2c59d81953adbb309
Celine-ST <celine.quek10@rafflesgirlssch.edu.sg>
2023-06-29 16:31:13 +0800

    finally got it working ^ o ^ added another example too just bcos

--- useless button/ContentView.swift
+++ useless button/ContentView.swift
@@ -17,24 +17,39 @@ struct ContentView: View {
     @State private var resetMoveNum = 0
     @State private var buttonClicked = false
     @State private var resetMoves = [
-        move(image: "catpaw", xOffset: 0.0, yOffset: 0.0, width: 100.0, height: 100.0, spin: 0)]
+        move(image: "catpaw", xOffset: 0.0, yOffset: 300.0, width: 0.0, height: 0.0, spin: 0),
+        move(image: "catpaw", xOffset: 0.0, yOffset: 0.0, width: 100.0, height: 100.0, spin: 0),
+        move(image: "waterdrop", xOffset: 0.0, yOffset: -490.0, width: 100.0, height: 100.0, spin: 0),
+        move(image: "waterdrop", xOffset: 0.0, yOffset: 0.0, width: 100.0, height: 100.0, spin: 0)]
     var body: some View {
         ZStack {
-                if buttonClicked {
-                    Image(resetMoves[resetMoveNum % resetMoves.count].image)
-                        .resizable()
-                        .frame(width: resetMoves[resetMoveNum % resetMoves.count].width, height: resetMoves[resetMoveNum % resetMoves.count].height)
-                        .offset(x: resetMoves[resetMoveNum % resetMoves.count].xOffset, y: resetMoves[resetMoveNum % resetMoves.count].yOffset)
-                }
+                Image(resetMoves[resetMoveNum % resetMoves.count].image)
+                    .resizable()
+                    .frame(width: resetMoves[resetMoveNum % resetMoves.count].width, height: resetMoves[resetMoveNum % resetMoves.count].height)
+                    .offset(x: resetMoves[resetMoveNum % resetMoves.count].xOffset, y: resetMoves[resetMoveNum % resetMoves.count].yOffset)
             Button {
                 withAnimation {
+                    resetMoveNum += 1
                     buttonClicked = true
                 }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                    withAnimation {
+                        resetMoveNum -= 1
+                    }
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
+                    resetMoveNum += 2
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
+                    withAnimation {
+                        buttonClicked = false
+                    }
+                }
             } label: {
-                Text("Click Me!\(resetMoveNum)")
+                Text(buttonClicked ? "Button Clicked!" : "Click Me!")
             }
             .padding()
-            .background(.blue)
+            .background(buttonClicked ? .cyan : .blue)
             .foregroundColor(.white)
         }
     }
