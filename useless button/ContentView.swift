256f960fb744401c92c024af5e9145926578ff6b
Celine-ST <celine.quek10@rafflesgirlssch.edu.sg>
2023-07-01 09:20:01 +0800

    emily's edits plus some of celine's edits cos emily had issues committing
    added some other images and animations

--- useless button/ContentView.swift
+++ useless button/ContentView.swift
@@ -15,42 +15,78 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var resetMoveNum = 0
+    @State private var yj = 0
     @State private var buttonClicked = false
     @State private var resetMoves = [
         move(image: "catpaw", xOffset: 0.0, yOffset: 300.0, width: 0.0, height: 0.0, spin: 0),
         move(image: "catpaw", xOffset: 0.0, yOffset: 0.0, width: 100.0, height: 100.0, spin: 0),
-        move(image: "waterdrop", xOffset: 0.0, yOffset: -490.0, width: 100.0, height: 100.0, spin: 0),
-        move(image: "waterdrop", xOffset: 0.0, yOffset: 0.0, width: 100.0, height: 100.0, spin: 0)]
+        move(image: "catpaw2", xOffset: 0.0, yOffset: -490.0, width: 100.0, height: 100.0, spin: 0),
+        move(image: "catpaw2", xOffset: 0.0, yOffset: 0.0, width: 100.0, height: 100.0, spin: 0),
+        move(image: "catpaw", xOffset: 0.0, yOffset: 300.0, width: 0.0, height: 0.0, spin: 0),
+        move(image: "catpaw", xOffset: 0.0, yOffset: 0.0, width: 100.0, height: 100.0, spin: 0),
+        move(image: "catpaw2", xOffset: 0.0, yOffset: -490.0, width: 100.0, height: 100.0, spin: 0),
+        move(image: "catpaw2", xOffset: 0.0, yOffset: 0.0, width: 100.0, height: 100.0, spin: 0),
+        move(image: "yj", xOffset: 0.0, yOffset: 0.0, width: 0.0, height: 0.0, spin: 0),
+        move(image: "yj", xOffset: 0.0, yOffset: 0.0, width: 300.0, height: 300.0, spin: 0)
+    ]
+    
+    private var colors = [Color.red, Color.green, Color.blue, Color.orange, Color.purple]
+    
+    private var randomBackgroundColor: Color {
+        Color(red: Double.random(in: 0...1), green: Double.random(in: 0...1), blue: Double.random(in: 0...1))
+    }
+    
+    private var randomTextColors: [Color] {
+        var colors = [Color]()
+        for _ in 0..<20 {
+            colors.append(Color(red: Double.random(in: 0...1), green: Double.random(in: 0...1), blue: Double.random(in: 0...1)))
+        }
+        return colors
+    }
+    
     var body: some View {
         ZStack {
-                Image(resetMoves[resetMoveNum % resetMoves.count].image)
+            randomBackgroundColor
+                .ignoresSafeArea()
+            
+            Image(resetMoves[resetMoveNum % resetMoves.count].image)
+                .resizable()
+                .frame(width: resetMoves[resetMoveNum % resetMoves.count].width, height: resetMoves[resetMoveNum % resetMoves.count].height)
+                .offset(x: resetMoves[resetMoveNum % resetMoves.count].xOffset, y: resetMoves[resetMoveNum % resetMoves.count].yOffset)
+            
+            if yj >= 20 {
+                Image("yj needs help")
                     .resizable()
-                    .frame(width: resetMoves[resetMoveNum % resetMoves.count].width, height: resetMoves[resetMoveNum % resetMoves.count].height)
-                    .offset(x: resetMoves[resetMoveNum % resetMoves.count].xOffset, y: resetMoves[resetMoveNum % resetMoves.count].yOffset)
-            Button {
-                withAnimation {
-                    resetMoveNum += 1
-                    buttonClicked = true
-                }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
-                    withAnimation {
-                        resetMoveNum -= 1
-                    }
-                }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
-                    resetMoveNum += 2
-                }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
-                    withAnimation {
-                        buttonClicked = false
-                    }
-                }
-            } label: {
-                Text(buttonClicked ? "Button Clicked!" : "Click Me!")
+                    .scaledToFit()
             }
-            .padding()
-            .background(buttonClicked ? .cyan : .blue)
-            .foregroundColor(.white)
+            else
+            {
+                Button {
+                    withAnimation {
+                        resetMoveNum += 1
+                        buttonClicked = true
+                        yj += 1
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                        withAnimation {
+                            resetMoveNum -= 1
+                        }
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
+                        resetMoveNum += 2
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
+                        withAnimation {
+                            buttonClicked = false
+                        }
+                    }
+                } label: {
+                    Text(buttonClicked ? "MEOW!" : "Meow")
+                        .padding()
+                        .background(buttonClicked ? colors[yj % colors.count] : randomBackgroundColor)
+                        .foregroundColor(buttonClicked ? randomTextColors[yj % randomTextColors.count] : Color(red: Double.random(in: 0...1), green: Double.random(in: 0...1), blue: Double.random(in: 0...1)))
+                }
+            }
         }
     }
 }
